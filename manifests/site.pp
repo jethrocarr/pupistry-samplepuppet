45600a338501698873364561c1ed3d7e5ffd9de2
Jethro Carr <jethro.carr@jethrocarr.com>
2015-04-10 18:53:14 +1200

    Should probably include the helper module

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,13 @@
 
 
 node default {
+
+  # Include the Pupistry helper module. You should load this on all masterless
+  # hosts, it fixes pluginsync for masterless and setups Pupistry background
+  # daemon to ensure you keep getting new artifacts applied.
+  include pupistry
+
+  # Show off a bit ;-)
   notify { 'Pupistry is fantastic, buy @jethrocarr beer!': }
 
   # Chuck some example modules in so the first Puppet run
